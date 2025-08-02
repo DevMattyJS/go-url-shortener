36a6fcc0b9c886a90e1fd0c5c71e5211c1e46d10
Matej Sliacky <matej.sliacky@protonmail.com>
2025-08-02 20:23:04 +0200

    Day 2: Variables and simple I/O - reading input from a user and printing it

--- main.go
+++ main.go
@@ -3,5 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, Go!")
+
+	const appName = "URL Shortener CLI"
+	var alias string
+	fullURL := ""
+
+	fmt.Println(appName)
+	fmt.Print("Please enter an alias for your URL:")
+	_, _ = fmt.Scanln(&alias)
+
+	fmt.Print("Please enter the full URL:")
+	_, _ = fmt.Scanln(&fullURL)
+
+	fmt.Printf("Saved URL for %s: %s\n", alias, fullURL)
 }
