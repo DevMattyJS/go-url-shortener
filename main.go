42f20205e6368046fcc5ff5ff9bdb63ae57b4a60
Matej Sliacky <matej.sliacky@protonmail.com>
2025-08-03 19:56:30 +0200

    Day 3: Implemented functions, control flow, and command-line argument handling

--- main.go
+++ main.go
@@ -1,19 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
-func main() {
-
+func runApp() {
 	const appName = "URL Shortener CLI"
 	var alias string
 	fullURL := ""
 
 	fmt.Println(appName)
-	fmt.Print("Please enter an alias for your URL:")
+	fmt.Print("Please enter an alias for your URL: ")
 	_, _ = fmt.Scanln(&alias)
 
-	fmt.Print("Please enter the full URL:")
+	fmt.Print("Please enter the full URL: ")
 	_, _ = fmt.Scanln(&fullURL)
 
 	fmt.Printf("Saved URL for %s: %s\n", alias, fullURL)
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		// No arguments provided, run the interactive app
+		runApp()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	case "add":
+		fmt.Println("Add command received.")
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+	}
 }
