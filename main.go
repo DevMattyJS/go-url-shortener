5c6804b766bbd579feef02f40a41d20e7586743e
Matej Sliacky <matej.sliacky@protonmail.com>
2025-08-06 21:01:13 +0200

    Added logic to store data in global map - will be updated to store in a file to be able to retrieve them

--- main.go
+++ main.go
@@ -5,22 +5,11 @@ import (
 	"os"
 )
 
-func main() {
+var urlMappings = make(map[string]URLMapping)
 
-	if len(os.Args) < 2 {
-		// No arguments provided, run the interactive app
-		runApp()
-		return
-	}
-
-	command := os.Args[1]
-
-	switch command {
-	case "add":
-		fmt.Println("Add command received.")
-	default:
-		fmt.Printf("Unknown command: %s\n", command)
-	}
+type URLMapping struct {
+	Alias   string
+	FullURL string
 }
 
 func runApp() {
@@ -36,4 +25,48 @@ func runApp() {
 	_, _ = fmt.Scanln(&fullURL)
 
 	fmt.Printf("Saved URL for %s: %s\n", alias, fullURL)
+}
+
+func main() {
+
+	if len(os.Args) < 2 {
+		// No arguments provided, run the interactive app
+		runApp()
+		return
+	}
+
+	command := os.Args[1]
+
+	switch command {
+	// Adds a new entry to urlMappings map if all parameters are received
+	case "add":
+
+		if len(os.Args) < 4 {
+			fmt.Println("Error: Missing paramaters! Usage: add <alias> <url>")
+			return
+		}
+
+		alias := os.Args[2]
+		fullURL := os.Args[3]
+		newEntry := URLMapping{Alias: alias, FullURL: fullURL}
+		urlMappings[alias] = newEntry
+		fmt.Printf("Added new URL mapping: %s -> %s\n", alias, fullURL)
+
+	// Retrieves an URL stored for an alias if all parameters are received
+	case "get":
+		if len(os.Args) < 3 {
+			fmt.Println("Error: Missing parameters! Usage: get <alias>")
+			return
+		}
+
+		alias := os.Args[2]
+		mapping, ok := urlMappings[alias]
+		if ok {
+			fmt.Printf("The full url for alias %s: %s", alias, mapping.FullURL)
+		} else {
+			fmt.Printf("Alias %s not found!", alias)
+		}
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+	}
 }
